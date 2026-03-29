bbcbd8e827f9da3e1825aa59edaa78bed8d70892
mayurnayak1705 <mayurnayak1705@gmail.com>
2026-03-29 22:19:37 +0530

    moe full implementation

--- moe.py
+++ moe.py
@@ -61,51 +61,105 @@ class Topk_noisy_Router(nn.Module):
 
     def forward(self, x):
         out_1 = self.router(x) #get the token projected to experts
-
         #generating noise and adding to the above output for the load balancing
         out_noise = self.noise(x)
         out_noise = torch.nn.functional.softplus(out_noise)
-        print(out_noise)
+        # print(out_noise)
         noise = torch.rand_like(out_noise)
         noise_add = out_noise * noise
         out_1 = out_1 + noise_add
-        #end of noise generation part
-        
+        #end of noise generation - addition part
+
         max_k, max_k_index = torch.topk(out_1, k=self.top_k, dim=-1) # find the top 2 highest percentage experts (top 2 values, thier index)
         gating = torch.full_like(out_1, float('-inf')) # create a 0 tensor of sf type
         sparse_top_k = gating.scatter(-1,max_k_index, max_k) #fill that tensor with -inf's everywhere except the top-2 positions of experts (keep thier values same)
         sf_sparse_topk = torch.nn.functional.softmax(sparse_top_k, dim=-1)
         return sf_sparse_topk, max_k_index #(batch, seq, emd)
     
-num_experts = 4
-top_k = 2
-n_embd = 32
-x = torch.rand(1, 10, 32)
-model = Topk_noisy_Router(d_model=n_embd, num_of_experts=num_experts, top_k=top_k)
-top_k_matrix, index = model(x)
+# num_experts = 4
+# top_k = 2
+# n_embd = 32
+x = torch.rand(2, 10, 32)
+# model = Topk_noisy_Router(d_model=n_embd, num_of_experts=num_experts, top_k=top_k)
+# top_k_matrix, index = model(x)
 # print(top_k_matrix)
 # print(index)
 
 
+class Expert(nn.Module):
+    def __init__(self, d_model):
+        super().__init__()
+        self.d_model = d_model
+        self.layer1 = nn.Linear(d_model, 4*d_model)
+        self.relu = nn.ReLU()
+        self.layer2 = nn.Linear(4*d_model, d_model)
+        self.dropout = nn.Dropout(p=0.1)
+    
+    def forward(self, x):
+        out = self.dropout(self.layer2(self.relu(self.layer1(x))))
+        return out
 
 
 
 
-class GPT_Transformer(nn.Module):
-    def __init__(self, model_dim, vocab_size, blocks):
+class SparseMOE(nn.Module):
+    def __init__(self, d_model, num_of_experts, top_k):
+        super().__init__()
+        self.d_model = d_model
+        self.num_of_experts = num_of_experts
+        self.topk = top_k
+        self.router = Topk_noisy_Router(d_model=self.d_model, num_of_experts=self.num_of_experts, top_k=self.topk)
+        self.experts = nn.ModuleList([Expert(self.d_model) for _ in range(num_of_experts)])
+
+    def forward(self, x):
+        gating_output, index = self.router(x)
+        final_output = torch.zeros_like(x)
+
+        flat_x = x.view(-1, x.size(-1))
+        # print(x.shape) #batch, seq, dim
+        # print(flat_x.shape) # batch * seq , dim
+        flat_gating_output = gating_output.view(-1, gating_output.size(-1))
+        # print(gating_output.shape) #batch,seq, experts
+        # print(flat_gating_output.shape) #batch*seq, experts
+        for i, expert in enumerate(self.experts):
+            expert_mask = (index== i).any(dim=-1) #checks if any token is assigned to the expert 'i'
+            flat_mask = expert_mask.view(-1) #shaping it to batch*seq , 
+            if flat_mask.any():
+                expert_input = flat_x[flat_mask] #get the seq tokens who has expert-i to be processed
+                logits = expert(expert_input)
+                # print([flat_mask, i])
+                # print(flat_gating_output.shape)
+                gating_scores = flat_gating_output[flat_mask, i].unsqueeze(1)
+                weighted_output = logits * gating_scores
+                final_output[expert_mask] += weighted_output.squeeze(1)
+                # print(expert_mask)
+        return final_output
+
+
+
+# model = SparseMOE(32, 10, 2)
+# print(model(x).shape)
+
+
+class MOE_Transformer(nn.Module):
+    def __init__(self, model_dim, vocab_size, head, blocks, num_of_experts, topk):
         super().__init__()
         self.blocks = blocks
+        self.head = head
+        self.num_of_experts = num_of_experts
+        self.topk = topk
         self.pos_encod = PositionalEncoding(d_model=model_dim, max_len=1000)
         self.model_dim = model_dim
         self.vocab_size = vocab_size
         self.embeddings = Embedding(vocab_size, model_dim)
-        self.mha = torch.nn.MultiheadAttention(model_dim, 12,batch_first=True)
+        self.mha = torch.nn.MultiheadAttention(model_dim, self.head,batch_first=True)
         self.linear1 = torch.nn.Linear(model_dim, model_dim * 4)
         self.linear2 = torch.nn.Linear(model_dim * 4, model_dim)
         self.relu = torch.nn.ReLU()
         self.norm1 = nn.LayerNorm(model_dim)
         self.norm2 = nn.LayerNorm(model_dim)
         self.llm_head = nn.Linear(self.model_dim, self.vocab_size)
+        self.moe_layer =SparseMOE(self.model_dim,self.num_of_experts,self.topk)
 
 
     def generate_causal_mask(self,seq_len):
@@ -124,17 +178,18 @@ class GPT_Transformer(nn.Module):
             x_in = self.norm1(x_in)
             attn_output, attn_weights  = self.mha(x_in,x_in,x_in,attn_mask=mask)
             #Mixture - of - Experts layer for FFN
-
-        
+            x_in = self.norm1(x_in + attn_output)
+            ff = self.moe_layer(x_in)
+            # print(f"shape after ffn {ff.shape}")
+            x_in = self.norm2(x_in + ff)
         logits = self.llm_head(x_in)
         prob = torch.softmax(logits, dim=-1)
         next_token = torch.argmax(prob, dim=-1)
         return next_token 
 
 
-
-
-
+model = MOE_Transformer(model_dim=32,vocab_size=vocabulary_size,head=8,blocks=8,num_of_experts=10,topk=2)
+print(model("Hi, my name"))
 
 
 
--- test.py
+++ test.py
@@ -1,4 +1,22 @@
 import torch
 
 x = torch.tensor([-0.0188, -0.3696, -0.1572, -0.2070,  0.3578, -0.4086,  0.0490,0.2428])
-print(torch.nn.functional.softplus(x))
+# print(torch.nn.functional.softplus(x))
+y = torch.tensor([[2, 3],
+        [3, 7],
+        [7, 4],
+        [7, 9],
+        [7, 6],
+        [7, 3],
+        [7, 3],
+        [3, 9],
+        [3, 1],
+        [0, 7]])
+mask = (y==7).any(dim=-1)
+m = mask.view(-1)
+v = torch.rand(20,32)
+y = torch.tensor([False, False, False, False,  True, False, False, False, False,  True,
+         True,  True, False,  True,  True,  True, False, False, False, False])
+
+
+print(v[y].shape)
